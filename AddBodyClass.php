51d5a305ce144b761c54bdb6a3153da39fab89be
Will Stott <willstott101@gmail.com>
2016-03-21 15:54:19 +0000

    typo

--- AddBodyClass.php
+++ AddBodyClass.php
@@ -1,7 +1,7 @@
 <?php
 /*
     Copyright 2012 Povilas Kanapickas <tir5c3@yahoo.co.uk>
-    Copyright 2016 Will Stott <willstott101@gamil.com>
+    Copyright 2016 Will Stott <willstott101@gmail.com>
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
